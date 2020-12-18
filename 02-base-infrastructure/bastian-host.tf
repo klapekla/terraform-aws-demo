fc71233e1fcf76aca5f73de848c35a595316cf1e
Klaus Klatt <kklatt@ovb.de>
2020-12-18 08:51:07 +0100

    added security group for bastion, and added region to user data script

--- 02-base-infrastructure/bastian-host.tf
+++ 02-base-infrastructure/bastian-host.tf
@@ -13,7 +13,9 @@ resource "aws_launch_template" "my_launch_template_for_bastion_host" {
   name_prefix   = "bastion_host"
   image_id      = "ami-0bd39c806c2335b95"
   instance_type = "t2.micro"
-  user_data     = filebase64(templatefile("bastion-host-eip-allocation.sh.tpl", {eip_public_ip = aws_eip.my_eip_for_bastion_host.public_ip , eip_allocation_id = aws_eip.my_eip_for_bastion_host.id }))
+  key_name      = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.my_ssh_sg.id]
+  user_data     = base64encode(templatefile("bastion-host-eip-allocation.sh.tpl", {region = var.region , eip_public_ip = aws_eip.my_eip_for_bastion_host.public_ip , eip_allocation_id = aws_eip.my_eip_for_bastion_host.id }))
 
   tags = {
     Name    = "my_launch_template_for_bastion_host"
@@ -30,6 +32,6 @@ resource "aws_autoscaling_group" "my_asg_for_bastion_host" {
 
   launch_template {
     id      = aws_launch_template.my_launch_template_for_bastion_host.id
-    version = "$Latest"
+    version = aws_launch_template.my_launch_template_for_bastion_host.latest_version
   }
 }
---
+++ 02-base-infrastructure/security-groups.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "my_ssh_sg" {
+  name        = "my_ssh_sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my_ssh_sg"
+    Project = var.project_tag
+  }
+}
