4b968fa13c07b65964449f67e2145c73a29231f3
Klaus Klatt <kklatt@ovb.de>
2020-12-20 13:14:45 +0100

    public ip for ec2 to be able to allocate eip

--- 02-base-infrastructure/bastion-host-eip-allocation.sh.tpl
+++ 02-base-infrastructure/bastion-host-eip-allocation.sh.tpl
@@ -2,5 +2,4 @@
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
   aws configure set default.region ${region}
   ec2_instance_id=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
-  aws ec2 wait instance-status-ok --instance-ids $ec2_instance_id
   aws ec2 associate-address --instance-id $ec2_instance_id --allocation-id ${eip_allocation_id}
--- 02-base-infrastructure/bastion-host.tf
+++ 02-base-infrastructure/bastion-host.tf
@@ -14,12 +14,17 @@ resource "aws_launch_template" "my_launch_template_for_bastion_host" {
   image_id      = "ami-0bd39c806c2335b95"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.my_key.key_name
-  vpc_security_group_ids = [aws_security_group.my_ssh_sg.id]
+
   iam_instance_profile {
     name = aws_iam_instance_profile.my_instance_profile.name
   }
   user_data     = base64encode(templatefile("bastion-host-eip-allocation.sh.tpl", {region = var.region , eip_public_ip = aws_eip.my_eip_for_bastion_host.public_ip , eip_allocation_id = aws_eip.my_eip_for_bastion_host.id }))
   update_default_version = true
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = [aws_security_group.my_ssh_sg.id]
+  }
 
   tags = {
     Name    = "my_launch_template_for_bastion_host"
